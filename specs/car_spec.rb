54476ad8eeb029f9aecff36f0e1182d78381b4a0
Tom Benham <benji26@hotmail.co.uk>
2016-10-04 09:38:42 +0100

    streamlined tests and made the code more dry

--- specs/car_spec.rb
+++ specs/car_spec.rb
@@ -7,38 +7,27 @@ class TestCar < MiniTest::Test
 
   def setup
     @car = Car.new("BMW")
-  end
-
-  def test_can_accelerate
-    @car.accelerate 
-    assert_equal(95, @car.fuel_level)
-    assert_equal(10, @car.speed)
-
   end 
 
-  def test_can_accelerate_twice
-    @car.accelerate 
-    @car.accelerate 
-    assert_equal(90, @car.fuel_level)
-    assert_equal(20, @car.speed)
+  def test_car_can_accelerate
+    5.times do
+      @car.accelerate 
+    end
+    assert_equal(75, @car.fuel_level)
+    assert_equal(50, @car.speed)
   end
 
-  def test_can_brake_once
-    @car.accelerate 
-    @car.accelerate 
-    @car.brake
-    assert_equal(10, @car.speed)
-  end
-
-  def test_can_brake_twice
-      @car.accelerate * 2
-      # @car.accelerate 
-      @car.brake * 2
-      # @car.brake
-      assert_equal(0, @car.speed)
+  def test_car_can_brake
+      4.times do
+        @car.accelerate
+      end
+      3.times do
+        @car.brake
+      end
+      assert_equal(10, @car.speed)
    end
 
-   def test_can_brake_from_zero
+   def test_car_can_brake_from_zero
       @car.brake
       assert_equal(0, @car.speed)
     end
